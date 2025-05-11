72d0b85380c2ee4e8c4ccea8ccf4723830d1b4c6
Theofilos Spyrou <Theofilos.Spyrou@gmail.com>
2025-05-11 15:36:17 +0200

    Create parametric example

--- examples/parametric.py
+++ examples/parametric.py
@@ -1,34 +1,91 @@
-import os
-import torch
+#############################################################################
+# An example to demonstrate a study of the parametric neuron faults for     #
+# various neuron parameters used by the SRM neurons.                        #
+#                                                                           #
+# In the beginning, the configuration parameters are specified to describe  #
+# the FI campaigns to be carried out. Namely, the 'layers' parameter sets   #
+# the layer(s) to target; 'params' selects the SRM neuron parameter(s) to   #
+# vary their value; and 'percent' defines the percentage of the faulty      #
+# value(s) relatively to the nominal one, which is indicated by the         #
+# network's yaml configuration file (e.g., demo -> config -> nmnist.yaml).  #
+# Next, the demo environment is prepared for the FI experiments, the        #
+# subject network is loaded, and a dataloader for the testing set is        #
+# created. Continuing with the main part of the example, a nested for-loop  #
+# iterates over all the combinations of the configuration parameters, where #
+# the corresponding FI campaigns are set up and executed. Each FI campaign  #
+# targets each of the neuron of the selected layer(s) individually, or in   #
+# other words, creates a separate fault round for each faulty location. In  #
+# addition, a sampling of maximum 250^2 neuron positions are to be injected #
+# in the case of very large layers, so that the experiment time is reduced. #
+# Those neuron locations are picked up randomly, while in the case that the #
+# targeted layer is composed of less neurons, all of them will be subject   #
+# of the FI experiments. After an FI campaign is over, the results are      #
+# stored in a file and the example continues with the next one. When all    #
+# the FI campaigns regarding the same SRM parameter finish, the results are #
+# visualized collectively for these experiments using a comparative plot of #
+# classification accuracy vs. parameter's deviation.                        #
+#############################################################################
+
 
 import slayerSNN as snn
-
 import spikefi as sfi
-import demo as cs
+import demo
 
 
-L = ['SC3']     # 'SF2', 'SF1', 'SC3', 'SC2', 'SC1', ''
-P = ['theta', 'tauSr', 'tauRef']
-C = range(10, 301, 10)  # Percentage of nominal value
+# Configuration parameters for the neuron parametric FI experiments
+# Select one or more layers to target (use an empty string '' to select all layers)
+layers = ['SC2']    # For example: 'SF2', 'SF1', 'SC3', 'SC2', 'SC1', ''
+# Select one or more neuron parameters to target
+params = ['theta']  # For example: 'theta', 'tauSr', 'tauRef'
+# Select the percentages of the parameter nominal values to set the faulty values
+percent = range(10, 301, 10)    # For example: from 10% to 300% with a step of 10%
 
-fnetname = cs.get_fnetname(trial='4')
-net: cs.Network = torch.load(os.path.join(cs.OUT_DIR, cs.CASE_STUDY, fnetname))
-net.eval()
+# Setup the fault simulation demo environment
+# Selects the case study, e.g., the LeNet network without dropout
+demo.prepare(casestudy='nmnist-lenet', dropout=False)
 
-cmpns_data = []
-for param in P:
-    for lay_name in L:
-        for c in C:
-            cmpn_name = fnetname.removesuffix('.pt') + f"_neuron_{param}_{lay_name or 'ALL'}_c{c}"
-            cmpn = sfi.Campaign(net, cs.shape_in, net.slayer, name=cmpn_name)
+# Load the network
+net = demo.get_net()
+# Create a dataset loader for the testing set
+test_loader = demo.get_loader('Test')
 
-            cmpn.inject_complete(sfi.fm.ParametricNeuron(param, c / 100.0), [lay_name])
+# Calculate total number of FI campaigns
+cmpns_total = len(params) * len(layers) * len(percent)
+cmpns_count = 0
 
-            print(cmpn.name)
-            cmpn.run(cs.test_loader, spike_loss=snn.loss(cs.net_params).to(cmpn.device))
-            print(f"{cmpn.duration : .2f} secs")
+# For each targeted parameter
+for param in params:
+    cmpns_data = []
+    # For each targeted layer
+    for lay_name in layers:
+        # For each faulty percentage of the nominal value
+        for cent in percent:
+            # Create a SpikeFI Campaign with a descriptive name
+            cmpn_name = demo.get_fnetname().removesuffix('.pt') + f"_neuron_{param}_{lay_name or 'ALL'}_c{cent}"
+            cmpn = sfi.Campaign(net, demo.shape_in, net.slayer, name=cmpn_name)
+            cmpns_count += 1
 
+            # Inject parametric neuron faults across all neurons of the layer
+            # with a faulty parameter value set to cent% of the nominal value
+            # Creates a separate fault round containing a single fault each
+            cmpn.inject_complete(sfi.fm.ParametricNeuron(param, cent / 100.0), [lay_name])
+
+            # Print status information
+            print(f"Campaign {cmpns_count}/{cmpns_total}: '{cmpn.name}'")
+
+            # Execute FI experiments for current targeted neuron parameter, layer, and faulty percentage
+            cmpn.run(test_loader, spike_loss=snn.loss(demo.net_params).to(cmpn.device))
+
+            # Print the duration of the FI campaign in seconds
+            print(f"Duration: {cmpn.duration : .2f} secs")
+
+            # Keep the campaign data aside to use later in the combinatorial plot
             cmpns_data.append(cmpn.export())
+
+            # Save the campaign results in a pkl file
             cmpn.save()
 
-    sfi.visual.plot(cmpns_data, xlabel=f'{param} (% of nominal value)', format='png')
+    # Visualize results as a function of the parameter's value deviation
+    # Creates one plot per neuron parameter
+    # The 'fig' object can be stored in a pickle file for later use/edit
+    fig = sfi.visual.plot(cmpns_data, xlabel=f"{param} (% of nominal value)", format='png')
